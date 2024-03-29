b5b94a0f794e166aefce52efd07e0ff3ee0caced
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-03-29 16:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function(){
+	const tp = document.getElementById('text').value;
+	const kd = parseInt(document.getElementById('delay').value);
+
+	await delayF(kd);
+	document.getElementById("output").innerText = tp;
+
+})
+
+function delayF(kd){
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve,kd);
+	})
+}
